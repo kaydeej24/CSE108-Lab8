852eafe953af3645ae236e4632a0bec2fbaeeb30
E.K. <fake.hack376@gmail.com>
2026-04-14 08:40:17 -0700

    sign in functionality

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, render_template, redirect, url_for, session
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_migrate import Migrate
-from models import db, User, Teacher, Class, Enrollment
+from models import db, User, Student, Teacher, Class, Enrollment
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -12,13 +13,13 @@ app.config['SECRET_KEY'] = 'secret'
 
 # init db
 db.init_app(app)
-
 # migrations
 migrate = Migrate(app, db)
 
 # admin
 admin = Admin(app, name="Dashboard")
 admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Student, db.session))
 admin.add_view(ModelView(Teacher, db.session))
 admin.add_view(ModelView(Class, db.session))
 admin.add_view(ModelView(Enrollment, db.session))
@@ -35,6 +36,50 @@ def home():
 def users():
     all_users = User.query.all()
     return render_template("users.html", users=all_users)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #email = request.form["email"]
+        password = request.form["password"]
+        username = request.form.get("username")
+
+        # 1. check if user exists
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            # LOGIN FLOW
+            if user.password == password:
+                session["user_id"] = user.id
+                return "Logged in successfully"
+            else:
+                return "Wrong password"
+
+        else:
+            # REGISTER FLOW (auto-create user)
+            #hashed_pw = generate_password_hash(password)
+
+            new_user = User(
+                username=username, #or email.split("@")[0],
+                #email=email,
+                password=password
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            session["user_id"] = new_user.id
+            return "User created and logged in"
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user_id" not in session:
+        return redirect(url_for("login"))
+
+    user = User.query.get(session["user_id"])
+    return f"Hello {user.username}"
 
 if __name__ == "__main__":
     app.run(debug=True)
